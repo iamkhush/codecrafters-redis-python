a4207bfa2714afdaabd6db50e8f4893ab59fe804
Ankush Chadda <ankush.chadda@klarna.com>
2023-02-06 23:44:11 +0100

    add buffer

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import socket
+from contextlib import closing
+from io import BytesIO
 
 
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-    server_socket.send("+PONG\r\n")
+    with closing(BytesIO()) as query_buffer:
+        query_buffer.write("+PONG\r\n")
+        server_socket.send(query_buffer.getvalue())
 
 
 if __name__ == "__main__":
