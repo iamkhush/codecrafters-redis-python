ab7261baa3b7ed6d122387808e248a1087add15a
Ankush Chadda <ankush.chadda@klarna.com>
2023-02-07 20:58:46 +0100

    fix send command

--- app/main.py
+++ app/main.py
@@ -6,11 +6,9 @@ from io import BytesIO
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-    with closing(BytesIO()) as query_buffer:
-        query_buffer.write(b"+PONG\r\n")
-        server_socket.send(query_buffer.getvalue())
-
+    connection, address = server_socket.accept() # wait for client
+    with connection:
+        connection.send(b'+PONG\r\n')
 
 if __name__ == "__main__":
     main()
