502c5bcb8378256bf6f7686e0381ae4e2835e99f
Ankush Chadda <ankush.chadda@klarna.com>
2023-02-07 21:18:28 +0100

    handle multiple pings

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 import socket
-from contextlib import closing
-from io import BytesIO
 
 
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
+    connection, _ = server_socket.accept() # wait for client
     with connection:
-        connection.send(b'+PONG\r\n')
+        while True:
+            data = connection.recv(1024)
+            if not data: break
+            connection.send(b'+PONG\r\n')
 
 if __name__ == "__main__":
     main()
