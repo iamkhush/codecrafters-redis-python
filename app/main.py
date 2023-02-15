f1c9a186639d9a581f4505c87fa6bcce2958fd66
Ankush Chadda <ankush.chadda@klarna.com>
2023-02-16 00:33:02 +0100

    used asyncio

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import socket
+import asyncio
 
+class EchoServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
 
-def main():
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(b'+PONG\r\n'))
+        self.transport.write(b'+PONG\r\n')
+
+async def main():
     print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    with connection:
-        while True:
-            data = connection.recv(1024)
-            if not data: break
-            connection.send(b'+PONG\r\n')
+    loop = asyncio.get_running_loop()
+    server = await loop.create_server(lambda: EchoServerProtocol(), 
+        "localhost", 6379, reuse_port=True)
+    async with server:
+        await server.serve_forever()
+    
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
